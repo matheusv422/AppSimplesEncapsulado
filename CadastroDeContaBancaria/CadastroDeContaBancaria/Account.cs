c9da70db36954e2c631c0b6bb3dc010d38c023eb
Matheus Vieira <matheusvr423@gmail.com>
2021-04-13 14:18:17 -0300

    Atividade simples de cadastro de conta bancária utlizando encapsulamento, construtores e properties.

---
+++ CadastroDeContaBancaria/CadastroDeContaBancaria/Account.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroDeContaBancaria
+{
+    class Account
+    {
+        public string Name { get; set; }
+        public int Number { get; private set; }
+        public double Balance { get; private set; }
+       
+
+      
+        public Account(string name, int number)
+        {
+            Name = name;
+            Number = number;
+            
+        }
+
+        public Account(string name, int number, double initDeposit) : this(name, number)
+        {
+
+            Deposit(initDeposit);
+        }
+        
+
+
+
+        
+
+
+
+
+        public int Numero
+        {
+            get { return Number; }
+        }
+
+
+        public double Saldo
+        {
+            get { return Balance; }
+           
+        }
+
+        public void Deposit (double deposit)
+        {
+            Balance += deposit;
+        }
+        
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+            Balance -= 5.00;
+
+        }
+
+        public override string ToString()
+        {
+            return   "Account "
+                    + Number
+                    + ", Holder: "
+                    + Name
+                    + ", Balance: $ "
+                    + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ CadastroDeContaBancaria/CadastroDeContaBancaria/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroDeContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Account ac;
+          
+            Console.Write("Enter account number: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter account holder: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Is there na initial deposit (y/n)? ");
+            char answer = char.Parse(Console.ReadLine());
+
+            
+            
+            
+            
+
+            if(answer == 'y')
+            {
+               
+                Console.Write("Enter initial deposit value: ");
+                double initDeposit = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                ac = new Account(nome, numero, initDeposit);
+            }
+
+            else
+            {
+                ac = new Account(nome, numero);
+               
+            }
+
+            Console.WriteLine("Account data:");
+            Console.WriteLine(ac);
+
+            Console.Write("Enter a deposit value: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            ac.Deposit(deposito);
+
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(ac);
+
+            Console.Write("Enter a withdraw value: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            ac.Withdraw(saque);
+
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(ac);
+
+
+
+
+
+
+
+
+        }
+    }
+}
